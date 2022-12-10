455df899d317720c69662d67404a126588905db1
mehdi-shamekhi <108692123+mehdi-shamekhi@users.noreply.github.com>
2022-12-10 12:36:54 -0500

    Update surface_adsorbate.py

--- surface_adsorbate.py
+++ surface_adsorbate.py
@@ -16,7 +16,27 @@ slab = slab * (4,4,1)
 # I recommend checking the Molecule class as well in ASE.
 adsorbate = Atoms("2N", positions=[[0,0,0], [0,0,1.1]])
 
+slab.set_calculator(calc)
+E_slab = slab.get_potential_energy()
 #Add adsorbate on the surface
 # Check out the attributes of add_adsorbate function
 #The default position is (0,0)
 add_adsorbate(slab, adsorbate, 2)
+
+calc = GPAW(mode=PW(400),
+            xc='BEEF-vdW',
+            kpts=(4, 4, 1))
+
+# Perform the DFT calculation for the clean surface:
+slab.set_calculator(calc)
+E_total = slab.get_potential_energy()
+
+# Perform the DFT calculation for the molecule adsorbed on the surface:
+adsorbate.set_calculator(calc)
+E_ads = adsorbate.get_potential_energy()
+
+# Calculate the adsorption energy:
+E_adsorption = E_total - E_slab - E_adsorbate
+
+
+
